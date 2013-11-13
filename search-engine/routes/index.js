199d2c32d273389a64f907b80d3d0c5db24fb56d
Delos Chang <delosfchang@gmail.com>
2013-11-13 15:46:14 -0500

    fixing up routing errors

--- search-engine/routes/index.js
+++ search-engine/routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'GroupMe Search Engine' });
 };
 
 exports.helloworld = function(req, res){
