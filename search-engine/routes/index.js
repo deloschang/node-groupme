c5c92468df2a425d02d05c9672b456176e471020
Delos Chang <delosfchang@gmail.com>
2013-11-13 20:24:43 -0500

    setting up POST request for search feature

--- search-engine/routes/index.js
+++ search-engine/routes/index.js
@@ -4,9 +4,17 @@
  */
 
 exports.index = function(req, res){
+  console.log(req);
   res.render('index', { title: 'GroupMe Search Engine'});
 };
 
-exports.helloworld = function(req, res){
-  res.render('helloworld', { title: 'Hello, World!' });
+exports.filterName = function(req, res){
+  // The query will be in req.body
+  var query = req.body['srch-term']
+  
+  if (query){
+    // Use the ScraperApp API to grab
+    var scraper = require('../ScraperApp');
+    res.render('index', { title: 'Searching '+query });
+  }
 };
